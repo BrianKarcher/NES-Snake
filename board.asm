7a85f8b3c44f4517a2f1fed860aa5dcf83fbb9fd
Brian Karcher <bkarcher@colony-games.com>
2024-07-27 03:07:06 -0700

    Better screen drawing routine

--- board.asm
+++ board.asm
@@ -1,13 +1,42 @@
 .include "constants.asm"
 ; Drawing the various game boards or levels
 
-.importzp zp_temp_1, zp_temp_2, start_low, start_high, current_low, current_high, end_low, end_high
+.importzp zp_temp_1, zp_temp_2, start_low, start_high, current_low, current_high, end_low, end_high, current_low_2, current_high_2
 .import screen
 .export draw_board
 
 board0:
     .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, $69, 0
     .byte 0, 0, 0, 0, $69, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, $70, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    .byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
 
 .proc draw_board
     ;rts
@@ -38,41 +67,111 @@ board0:
 
 load_board_to_nt:
     lda #<board0
-    sta start_low
+    ;sta start_low
     sta current_low
     lda #>board0
-    sta start_high
+    ;sta start_high
     sta current_high
-    lda #<board0
-    clc
-    adc #$40
-    sta end_low
-    lda #>board0
-    adc #$00 ; add carry flag, if needed
-    sta end_high
 
+    lda #<screen
+    sta current_low_2
+    lda #>screen
+    sta current_high_2
+
+    ;lda #<board0
+    ;clc
+    ;adc #$40
+    ;sta end_low
+    ;lda #>board0
+    ;adc #$00 ; add carry flag, if needed
+    ;sta end_high
+
+    ;ldy #$0
+    ldx #$0
     ldy #$0
-    loop:
+
+    ; x and y are flip flopped because indirect indexing forces us to use y :(
+    fory:
+    ldy #$0
+    forx:
+    ; loop:
     ; Transfer the current memory location to the PPU
     lda (current_low), y
     sta PPU_DATA
     ; Increment the address
     ;iny
-    inc current_low
-    bne @no_high_increment
-        inc current_high
-        lda #$00
-        sta current_low
-    @no_high_increment:
-        ; Check if we've reached the end
-        lda current_low
-        cmp end_low ; Check low first since it is more likely to be different
-        bne loop
-        lda current_high
-        cmp end_high
-        bne loop
+    ;inc current_low
+    iny
+    jmp checkexit
+    noexit:
+    cpy #$FF ; 32
+    bne forx
+
+    inc current_high
+    inx
+    cpx #$1e ; 30
+    bne fory
+
+    checkexit:
+    cpy #$c0
+    bne noexit
+    cpx #$03
+    bne noexit
+    rts
+    ;bne @no_high_increment
+    ;    inc current_high
+    ;    inc ldx
+        ;lda #$00
+        ;sta current_low
+    ; @no_high_increment:
+    ;     ; Check if we've reached the end
+    ;     lda current_low
+    ;     cmp end_low ; Check low first since it is more likely to be different
+    ;     bne loop
+    ;     lda current_high
+    ;     cmp end_high
+    ;     bne loop
         ; Done transfering
-        rts
+    
+
+
+; load_board_to_nt:
+;     lda #<board0
+;     sta start_low
+;     sta current_low
+;     lda #>board0
+;     sta start_high
+;     sta current_high
+;     lda #<board0
+;     clc
+;     adc #$40
+;     sta end_low
+;     lda #>board0
+;     adc #$00 ; add carry flag, if needed
+;     sta end_high
+
+;     ldy #$0
+;     loop:
+;     ; Transfer the current memory location to the PPU
+;     lda (current_low), y
+;     sta PPU_DATA
+;     ; Increment the address
+;     ;iny
+;     inc current_low
+;     bne @no_high_increment
+;         inc current_high
+;         lda #$00
+;         sta current_low
+;     @no_high_increment:
+;         ; Check if we've reached the end
+;         lda current_low
+;         cmp end_low ; Check low first since it is more likely to be different
+;         bne loop
+;         lda current_high
+;         cmp end_high
+;         bne loop
+;         ; Done transfering
+;         rts
 
 .proc draw_horizontal
 
